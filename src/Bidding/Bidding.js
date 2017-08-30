f61eb664bdb1e6922622b57a5f129a61ab35ec2a
Mark Redeman <markredeman@gmail.com>
2017-08-30 13:14:10 +0200

    Improve styling of bid screen

--- src/Bidding/Bidding.js
+++ src/Bidding/Bidding.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
 import { Spade, Heart, Diamond, Club, Sans } from '../Suits.js';
-import 'react-select/dist/react-select.css';
 
 export default class Bidding extends Component {
 
@@ -14,7 +12,7 @@ export default class Bidding extends Component {
             suit: "spades",
             bid: 80,
             team: "we",
-        }
+        };
     }
 
     selectTeam(team) {
@@ -67,72 +65,111 @@ export default class Bidding extends Component {
         });
     }
 
+    suits() {
+        return [
+            { value: 'spades', label: <span><Spade />{'Spades'}</span> },
+            { value: 'hearts', label: <span><Heart />{'Hearts'}</span> },
+            { value: 'diamonds', label: <span><Diamond />{'Diamonds'}</span> },
+            { value: 'clubs', label: <span><Club />{'Clubs'}</span> },
+            { value: 'sans', label: <Sans /> },
+        ]
+    }
+
     render() {
         let that = this;
 
-        return (
-                <div className="card my-4">
+        let ChooseTeam = () => {
+            return <div className="my-3">
                 <div className="card-body">
-                <h2>
-                Submit the bid by
-            </h2>
-
-                <div className="row">
-                <div className="col-md-6">
-                <button
-            type="button"
-            className={"my-1 btn btn-lg btn-block " + (this.state.team === "we" ? "btn-primary" : "btn-outline-primary")}
-            onClick={ () => that.selectTeam('we') }
-                >
-                We ({this.players('we').join(' and ')})
-            </button>
+                    <div className="card-title">
+                        <h3>Who's playing?</h3>
+                    </div>
                 </div>
-                <div className="col-md-6">
-                <button
-            type="button"
-            className={"my-1 btn btn-lg btn-block " + (this.state.team === "they" ? "btn-primary" : "btn-outline-primary")}
-            onClick={ () => that.selectTeam('they') }
-                >
-                They ({this.players('they').join(' and ')})
-            </button>
-                </div>
-                </div>
-                <div className="form-group my-4">
-                <Select
-            name="choose-suit"
-            value={that.state.suit}
-            options={[
-                { value: 'spades', label: <span><Spade />{'Spades'}</span> },
-                { value: 'hearts', label: <span><Heart />{'Hearts'}</span> },
-                { value: 'diamonds', label: <span><Diamond />{'Diamonds'}</span> },
-                { value: 'clubs', label: <span><Club />{'Clubs'}</span> },
-                { value: 'sans', label: <Sans /> },
-            ]}
-            onChange={that.selectSuit}
-            searchable={false}
-            clearable={false}
-                />
-                </div>
-                <h3 className="text-center my-3">Choose your bid</h3>
-                <div className="row">
-                {this.possibleBids().map((bid) => {
-                    return (
-                            <div className="col-6 col-sm-4 col-md-3 my-3">
-                            <button
-                        className={"btn btn-lg btn-block " + (this.state.bid === bid ? "btn-primary" :  "btn-outline-primary")}
-                        onClick={() => that.selectBid(bid)}
-                            >
-                            {bid}
+                <div className="row no-gutters">
+                    <div className="col-md-6">
+                        <button
+                            type="button"
+                            className={"rounded-0 btn btn-lg btn-block " + (this.state.team === "we" ? "btn-primary" : "btn-outline-primary")}
+                            onClick={ () => that.selectTeam('we') }
+                        >
+                            We ({this.players('we').join(' and ')})
                         </button>
-                            </div>
-                    );
-                })}
-            </div>
-                <button className="btn btn-lg btn-block btn-outline-success" onClick={that.submitBid}>
-                Submit bid
-            </button>
-            </div>
+                    </div>
+                    <div className="col-md-6">
+                        <button
+                            type="button"
+                            className={"rounded-0 btn btn-lg btn-block " + (this.state.team === "they" ? "btn-primary" : "btn-outline-primary")}
+                            onClick={ () => that.selectTeam('they') }
+                        >
+                            They ({this.players('they').join(' and ')})
+                        </button>
+                    </div>
                 </div>
+            </div>
+        };
+
+        let ChooseSuit = () => {
+            return <div className="my-3">
+                <div className="card-body">
+                    <div className="card-title">
+                        <h3>Choose a suit</h3>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    {this.suits().map((suit) => {
+                         return <div className="col">
+                             <button
+                                 className={"rounded-0 btn btn-block btn-lg " + (this.state.suit === suit.value ? "btn-primary" : "btn-outline-primary")}
+                                 onClick={() => that.selectSuit({ value: suit.value })}
+                             >
+                                 { suit.label }
+                             </button>
+                         </div>
+                    })}
+                </div>
+            </div>
+        };
+
+        let ChooseBid = () => {
+            return <div className="my-3">
+                <div className="card-body">
+                    <div className="card-title">
+                        <h3>Choose a bid</h3>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    {this.possibleBids().map((bid) => {
+                         return (
+                             <div className="col-6 col-sm-4 col-md-3">
+                                 <button
+                                     className={"rounded-0 btn btn-lg btn-block " + (this.state.bid === bid ? "btn-primary" :  "btn-outline-primary")}
+                                     onClick={() => that.selectBid(bid)}
+                                 >
+                                     {bid}
+                                 </button>
+                             </div>
+                         );
+                    })}
+                </div>
+            </div>
+        };
+
+        return (
+            <div className="card my-4">
+                <div className="card-header">
+                    <h2>
+                        Submit a new bid
+                    </h2>
+                </div>
+
+                <ChooseSuit />
+                <ChooseBid />
+                <ChooseTeam />
+
+                <button className="rounded-0 btn btn-lg btn-block btn-outline-success" onClick={that.submitBid}>
+                    Submit bid
+                </button>
+            </div>
         )
     }
 };
