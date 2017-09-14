d937663f4a0182edea858de0a5fbf05f9a9beb90
Mark Redeman <markredeman@gmail.com>
2017-09-14 19:10:25 +0200

    Improve styling of completing a hand
    - Add fontawesome icons for counting, pit and wet
    - Improve responsive design

--- src/PlayHand/CompleteHand.js
+++ src/PlayHand/CompleteHand.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 
 export default class CompleteHand extends Component {
     render() {
@@ -9,32 +10,37 @@ export default class CompleteHand extends Component {
         return (
             <ul className="list-unstyled row mb-0">
                 <li className="col-sm-12 col-md-3 d-flex align-items-center">
-                    <h4>
+                    <h4 className="h6">
                         {this.props.title}
                     </h4>
                 </li>
 
                 {/* Unless a the bid was pid, since counting */}
-                <li className="col-sm-4 col-md-3 d-flex align-items-center">
+                <li className="col-4 col-md-3 d-flex align-items-center">
                     <button className={btnStyleSuccess} onClick={this.props.countPoints}>
-                        Count our points
+                        <FontAwesome name="hand-spock-o" className="mr-2" />
+                        Count
                     </button>
                 </li>
 
                 { // If the other team earned fame then they won at least one round, so
                   // no pit can be earned
                     ! this.props.enablePit ? '' :
-                    <li className="col-sm-4 col-md-3 d-flex align-items-center">
+                    <li className="col-4 col-md-3 d-flex align-items-center">
                         <button className={btnStylePit} onClick={this.props.confirmPit}>
-                            received a {this.props.isPlaying ? '' : 'anti'} Pit
+                            <FontAwesome name="hand-stop-o" className="mr-2" />
+                            {this.props.isPlaying ? 'Pit' : 'Anti pit'}
                         </button>
                     </li>
                 }
 
                 {
                     ! this.props.isPlaying ? '' :
-                    <li className="col-sm-4 col-md-3 d-flex align-items-center">
-                        <button className={btnStyleWet} onClick={this.props.confirmWet}>went wet</button>
+                    <li className="col-4 col-md-3 d-flex align-items-center">
+                        <button className={btnStyleWet} onClick={this.props.confirmWet}>
+                            <FontAwesome name="tint" className="mr-2" />
+                            Wet
+                        </button>
                     </li>
                 }
             </ul>
