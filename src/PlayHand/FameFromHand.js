d03eb1bfc11a0937f4a43c60145f869d09a96724
Mark Redeman <markredeman@gmail.com>
2017-09-14 19:07:03 +0200

    Improve styling of adding fame to hand
    - Use FontAwesome for undoing and resetting fame
    - Show undo and reset fame options after adding fame options, and only if at
      least one fame was recorded

--- src/PlayHand/FameFromHand.js
+++ src/PlayHand/FameFromHand.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import AddFameToHand from './AddFameToHand.js';
+import FontAwesome from 'react-fontawesome';
 
 export default class FameFromHand extends Component {
     render() {
-        let undoFameStyle = "btn btn-lg btn-link " + (this.props.lastFame ? "text-dark" : "text-light");
-        let players = this.props.players.join(' and ');
+        let undoFameStyle = "btn btn-link text-dark";
 
         return (
             <div>
-                <div className="row mt-3">
-                    <div className="col-sm-3">
-                        <h3>Add fame</h3>
-                    </div>
-
-                    <div className="col-sm-9 text-right">
-                        {! this.props.lastFame ? '' :
-                         <button className={undoFameStyle} onClick={this.props.undoLastFame}>
-                             Undo last fame ({this.props.undoFameAmount} fame by {players})
-                         </button>
-                        }
-                        <button className="btn btn-lg btn-link text-danger" onClick={this.props.resetFame}>Reset fame to 0</button>
-                    </div>
-                </div>
-
+                <h3 className="h5">Add fame</h3>
                 <AddFameToHand team="We" fame={this.props.fameByUs} addFame={this.props.addFameToUs} />
                 <AddFameToHand team="They" fame={this.props.fameByThem} addFame={this.props.addFameToThem} />
+
+                {! this.props.lastFame ? '' :
+                 <div className="row mt-3 text-center">
+                     <div className="col">
+                         <button className={undoFameStyle} onClick={this.props.undoLastFame}>
+
+                             <FontAwesome name="undo" className="mr-2" />
+                             Undo last fame
+                         </button>
+                     </div>
+                     <div className="col">
+                         <button className="btn btn-link text-danger" onClick={this.props.resetFame}>
+                             <FontAwesome name="refresh" className="mr-2" />
+                             Reset all fame to 0
+                         </button>
+		                 </div>
+                 </div>
+                }
             </div>
         )
     }
